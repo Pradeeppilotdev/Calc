631da869314df00d2c0b3c8be77c049a3b5f6053
Pradeeppilot <pradeeppilotdev@gmail.com>
2025-01-08 21:52:33 +0530

    calc

---
+++ script.js
@@ -0,0 +1,22 @@
+// simple calculator
+const display = document.getElementById('display')
+
+function appendToDisplay(input){
+    display.value += input;
+
+}
+
+function ClearDisplay(){
+    display.value="";
+
+}
+
+function Calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Oops Error"
+    }
+
+}
